4511cf5082750549d00c087099cd6813fb25e0e9
236780 <236780@student.pwr.edu.pl>
2020-06-05 11:55:42 +0200

    add initialization

--- ising3D/ising3D.cpp
+++ ising3D/ising3D.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <assert.h>
+#include <random>
+#include <time.h>
 using namespace std;
 
 // Parametry:
@@ -10,22 +12,41 @@ double T;
 uint next(uint i){return i==L-1 ? 0 : i+1;}
 uint prev(uint i){return i==0 ? L-1 : i-1;}
 
-double dE(uint i, uint j, uint k)
-{
-    
-}
+// Gener. liczb pseudolosowych:
+default_random_engine rng_engine;
+uniform_real_distribution<double> rng_distribution(0.0,1.0);
+inline double rnd() {return rng_distribution(rng_engine);}
 
-int main (int argc, char** argv)
+double dE(uint i, uint j, uint k);  // TODO
+
+int main(int argc, char** argv)
 {
-    // Wczytaj parametry:
+    // Ustawienie ziarna RNG:
+    int rng_seed;
+    if (argc > 1) rng_seed = stoi(argv[1]); //Jezeli uzytkownik nie podal ziarna przy
+    else rng_seed = time(nullptr);          //uruchomieniu programu, uzyj obecnego czasu
+
+    // Wczytaj parametry z wej. standardowego:
     cin >> L >> mcs >> T;
     cout << "# L = " << L << ", T = " << T << ", " << mcs << " MCS" << endl;
+    cout << "# seed: " << rng_seed;
 
     // Inicjalizacja tablicy:
     char spin[L][L][L];
     char newspin[L][L][L];
-    
 
+    // Losowanie konf. poczatkowej
+    for(uint i=0;i<L;i++)
+    {
+        for(uint j=0;j<L;j++)
+        {
+            for(uint k=0;k<L;k++)
+            {
+                char rand_spin = rnd()<0.5 ? -1 : 1;
+                spin[i][j][k] = rand_spin;
+            }
+        }
+    }
 
     return 0;
 }
